9958f3ae47e0253f4d488178505ba2c4d321ea0c
小胡同学 <123156213+PuPuoo@users.noreply.github.com>
2023-04-11 14:12:54 +0800

    Create 01-变量和简单数据类型.py

---
+++ 01-变量和简单数据类型.py
@@ -0,0 +1,29 @@
+# test
+print("hello world!")
+
+# 变量
+message = "hello world!"
+print(message)
+
+# 修改字符串大小写
+name = "ada lovezy"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+# 字符串合并
+first = "hu"
+last = "qingying"
+full = first + " " + last
+print(full)
+
+# 添加换行
+print("hey\n\they\n\they")
+
+# 删除空白
+lan = "python "
+print(lan.rstrip())
+
+# 字符串+数字
+message = "hqy" + str(13) + "zy"
+print(message)
